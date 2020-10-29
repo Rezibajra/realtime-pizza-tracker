282b1917498aacb7f5c47a95a8c11ca9045482a1
Rezibajra <rezibajjra@gmail.com>
2020-10-29 17:59:47 +0545

    Implemented cookies and sessions

--- app/http/controllers/homeController.js
+++ app/http/controllers/homeController.js
@@ -1,11 +1,10 @@
 const Menu =require('../../models/menu');
 function homeController(){
     return{
-        index(req,res) {
-            Menu.find().then(function(pizzas){
-                console.log(pizzas)
-                return res.render('home',{pizzas:pizzas})
-            })
+        async index(req,res) {
+            const pizzas=await Menu.find()
+            console.log(pizzas)
+            return res.render('home',{pizzas:pizzas})
         }
     }
 }
