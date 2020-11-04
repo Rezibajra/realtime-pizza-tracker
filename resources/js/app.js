31dff4f68d02a7b657850923b7995743b63fc8f2
Rezibajra <rezibajjra@gmail.com>
2020-11-04 21:43:38 +0545

    Initial stripe setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import Noty from 'noty'
 import { initAdmin } from './admin'
 import moment from 'moment'
+import { initStripe } from './stripe'
 
 let addToCart=document.querySelectorAll('.add-to-cart')
 let cartCounter=document.querySelector('#cartCounter')
@@ -71,6 +72,9 @@ function updateStatus(order){
 }
 
 updateStatus(order);
+
+initStripe()
+
 
 //socket
 let socket=io()
---
+++ resources/js/stripe.js
@@ -0,0 +1,63 @@
+import { loadStripe } from '@stripe/stripe-js'
+import { placeOrder } from './apiService'
+
+export async function initStripe(){
+    const stripe = await loadStripe('pk_test_51HjflTE4hg7Z31vPKj8whzq74ruoKQlFtf1oeZ3BUkyYffTi81hg4Orzqa9LS1kRKHa0ap0s1kHUmY8Y3luNkyNz00o9KvITeA');
+    let card = null;
+    function mountWidget(){
+        const elements = stripe.elements()
+
+            let style = {
+                base: {
+                    color: '#32325d',
+                    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+                    fontSmoothing: 'antialiased',
+                    fontSize: '16px',
+                    '::placeholder': {
+                    color: '#aab7c4'
+                    }
+                },
+                invalid: {
+                    color: '#fa755a',
+                    iconColor: '#fa755a'
+                }
+            };
+
+            card = elements.create('card', { style,hidePostalCode: true})
+            card.mount('#card-element')
+    }
+    const paymentType = document.querySelector('#paymentType')
+    if(!paymentType){
+        return;
+    }
+    paymentType.addEventListener('change',(e) =>  {
+        if(e.target.value === 'card'){
+            mountWidget()
+        }else{
+            card.destroy()
+        }
+    })
+//ajax call
+const paymentForm = document.querySelector('#payment-form');
+if(paymentForm){
+    paymentForm.addEventListener('submit',(e)=>{
+        e.preventDefault();
+        let formData = new FormData(paymentForm)
+        let formObject = {}
+        for(let [key,value] of formData.entries()){
+            formObject[key] = value
+        }
+
+        if(!card){
+            placeOrder(formObject);
+            return;
+        }
+        //verify card 
+        stripe.createToken(card).then((result) => {
+            placeOrder(formObject);
+        }).catch((err) => {
+            console.log(err)
+        })
+    })
+}
+}
